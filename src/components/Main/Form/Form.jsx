fc35df32e16ad2ec55e26a248bd7afe7d27ea558
gonzalogonzalo <calleriza.gonzalo@gmail.com>
2022-02-20 20:16:58 +0100

    Finished formating date

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -3,6 +3,7 @@ import { TextField, Typography, Grid, Button, FormControl, InputLabel, Select, M
 import { ExpenseTrackerContext } from '../../../context/context.js';
 import { v4 as uuidv4} from 'uuid';
 
+import formatDate from '../../../utils/formatDate.js';
 import useStyles from './styles.js'
 import { incomeCategories, expenseCategories } from '../../../constants/categories.js'; 
 
@@ -10,7 +11,7 @@ const initialState = {
     amount: '',
     category: '',
     type: 'Income',
-    date: new Date(),
+    date: formatDate(new Date()),
 }
 
 const Form = () => {
@@ -60,7 +61,7 @@ const Form = () => {
                 <TextField type='number' label='Amount' fullWidth value={formData.amount} onChange={(e) => setFormData({ ...formData, amount: e.target.value})} />
             </Grid>
             <Grid item xs={6}>
-                <TextField type='date' label='Date' fullWidth value={formData.data} onChange={(e) => setFormData({ ...formData, data: e.target.value })} />
+                <TextField type='date' label='Date' fullWidth value={formData.data} onChange={(e) => setFormData({ ...formData, data: formatDate(e.target.value) })} />
             </Grid>
             <Button className={classes.button} variant='outlined' color='primary' fullWidth onClick={createTransaction}>Create</Button>
         </Grid>
