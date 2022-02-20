83cf1a33f39a17ceeeac798b0d461d933579cd2c
gonzalogonzalo <calleriza.gonzalo@gmail.com>
2022-02-20 22:38:17 +0100

    Finished APP, need to check some styling

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useContext} from 'react';
 import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@mui/material';
 
+import { ExpenseTrackerContext } from '../../context/context.js';
 import useStyles from './styles.js';
 import Form from './Form/Form.jsx';
 import List from './List/List.jsx';
 
 const Main = () => {
+
+    const { balance } = useContext(ExpenseTrackerContext);
+
     const classes = useStyles
     return (
         <Card className={classes.root}>
             <CardHeader title='Expense Tracker' subheader='Powered by Speechly' />
             <CardContent>
-                <Typography align='center' variant='h5'>Total Balance $100</Typography>
+                <Typography align='center' variant='h5'>Total Balance ${balance}</Typography>
                 <Typography variant='subtitle1' style={{ lineHeight: '1.5em', marginTop: '20px'}}>
                     Try saying: add income in ....
                 </Typography>
